d3b63dc2f84667f724af85b53c44fe90f3056997
Daegeun Jeong <wjdeorms27@naver.com>
2022-09-12 21:00:21 +0900

    Feat : product model 생성

--- product/models.py
+++ product/models.py
@@ -6,5 +6,24 @@ class Category(models.Model):
     
     class Meta:
         db_table = "categories"
+        
+    def __str__(self):
+        return self.category_name
 
 
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    category_name = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = "products"
+        
+    def __str__(self):
+        return self.product_name
